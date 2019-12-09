bc1526d0dac9834a2fc7e6d4c85e9892aed54db9
theo.martos@hotmail.fr <43QrcuprdlvQacQ9>
2019-12-09 20:11:16 +0100

    Updated the app and the README

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 # GNS3 WebServer
 
-This repo is a docker container creater to provide a custom HTTP(S) web server. It contain a simple PHP form and will be used to demonstrate some network vulnerability.
+This repo is a docker container created to provide a custom HTTP(S) web server. It contain a simple PHP form and will be used to demonstrate some network vulnerabilities.
+
+The credentials for the form are :
+
+    username : admin
+    password : securePassword
 
 # Credit
 
-This docker container is built on top of the webdevops/php-nginx container that provide the PHP/Nginx engine, based on alpine OS.
+This docker container is built on top of the [webdevops/php-nginx](https://hub.docker.com/r/webdevops/php-nginx/) container that provide the PHP/Nginx engine, based on alpine OS.
+
+# Licence
+
+This tool is provided under the MIT Licence :
+
+```
+MIT License
+
+Copyright (c) 2019 Théo Martos
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
--- app/index.php
+++ app/index.php
@@ -1,50 +1,76 @@
 <?php
     session_start();
  
-    if (isset($_POST['username'], $_POST['password'])) {
-        $_SESSION['username'] = htmlspecialchars($_POST['username']);
-    }
+    const USERNAME = "admin";
+    const PASSWORD = "securePassword";
 
-    $logged = isset($_SESSION['username']) ? true : false;
+    $_SESSION['logged'] = isset($_SESSION['logged']) ? $_SESSION['logged'] : false;
+    $login_error = NULL;
+
+    if (isset($_POST['username'], $_POST['password']))
+    {
+        if ($_POST['username'] === USERNAME and $_POST['password'] === PASSWORD)
+        {
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['logged'] = true;
+        }
+        else
+        {
+            $login_error = "Bad credentials";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Docker</title>
+        <title>Web App</title>
     </head>
     
     <body>
-        <h1>Login page</h1>
-        <?php if ($logged) { ?>
-            <div>
-                Hello <?php echo $_SESSION['username'].PHP_EOL; ?>
-            </div>
-            <br>
-            <br>
-            <a href="logout.php"><button>Logout</button></a>
+        <?php if ($_SESSION['logged']) { ?>
+            <section>
+                <h2>Hello <?php echo $_SESSION['username'].PHP_EOL; ?></h2>
+                <br>
+                <br>
+                <a href="logout.php"><button>Logout</button></a>
+            </section>
         <?php }  else { ?>
+            <h1>Login page</h1>
+
             <form method="post" action="index.php">
                 <div id="container">
                     <div id="labels">
-                        <label for="username">Username :</label><br>
+                        <label for="username" style="margin-bottom: 10px;">Username :</label><br>
                         <label for="password">Password :</label><br>
                     </div>
 
                     <div id="inputs">
-                        <input name="username" type="text" id="username"><br>
-                        <input name="password" type="password" id="password"><br>
+                        <input name="username" type="text" id="username" required><br>
+                        <input name="password" type="password" id="password" required><br>
                     </div>
                 </div>
-                <input type="submit" value="Login">
+                <input type="submit" id="login" value="Login">
             </from>
+            <?php if(!is_null($login_error)) {
+                echo "<br><br><span id=\"error\">{$login_error}</span>".PHP_EOL;
+            } ?>
         <?php } ?>
     </body>
 
     <style>
         h1 {
-            text-align: center
+            text-align: center;
+        }
+
+        section {
+            text-align: center;
+        }
+
+        form {
+            width: 355px;
+            margin: auto;
         }
 
         form #container {
@@ -55,8 +81,20 @@
             width: 350px;
         }
 
-        #container label, #container input {
+        label {
+            display: inline-block;
+        }
+
+        #container input {
             margin-bottom: 5px;
+        }
+
+        #login {
+            width: 350px;
+        }
+
+        #error {
+            color: red;
         }
     </style>
 </html>
